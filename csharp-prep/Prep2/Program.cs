ff957b934fd9a25ea6b83f426ea4dc68c02fd70f
Emily Jena <jen21062@byui.edu>
2026-02-21 15:30:27 -0700

    Completed the core requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string grade = Console.ReadLine();
+        float finalgrade = float.Parse(grade);
+
+        string letter = "";
+            if (finalgrade >= 90)
+            {
+                letter = "A";
+            }
+            else if (finalgrade >= 80)
+            {
+                letter = "B";
+            }
+            else if (finalgrade >= 70)
+            {
+                letter = "C";
+            }
+            else if (finalgrade >= 60)
+            {
+               letter = "D";
+            }
+            else
+            {
+                letter = "F";
+            }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+        
     }
 }
