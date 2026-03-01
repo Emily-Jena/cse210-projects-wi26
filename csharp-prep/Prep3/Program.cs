69b648eece0807bbe9cc0e06f7b53cd49326e62d
Emily Jena <jen21062@byui.edu>
2026-02-28 18:32:57 -0700

    changes made to the Lines.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Formats.Asn1;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+
+        do {
+        // Generating random numbers - computer assisted
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int response = 0;
+        int count = 0; // start of the count
+
+        while (response != number)
+            {
+
+            Console.Write("What is the magic number? ");
+            response = int.Parse(Console.ReadLine());
+            count++; //increment the guessing
+
+            if (response > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+            else if (response < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+            else
+                {
+                    Console.WriteLine($"Congratulations, You guessed it in {count}!");
+                }
+            } 
+        Console.Write("Want to play again? (yes/no): ");
+        playAgain = Console.ReadLine().ToLower();
+        } while (playAgain == "yes");
     }
-}
+} 
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,32 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    // static void Main(string[] args)
+    // {
+    //     //Console.WriteLine("Hello Sandbox World!");
+
+    //     for (int i = 0; i < 10; i++)
+    //     {
+    //         Console.WriteLine(i);
+    //     }
+    // }
+
+// How to write a funtion
+    public static void Greeting()
     {
-        Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Hey Emily");
+    }
+// Returning a function 
+    public static int AddNumbers(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+
+// Calling a function 
+static void Main(string[] args)
+    {
+        Greeting();
+        int total = AddNumbers(10,20);
+        Console.WriteLine(total);
     }
 }
