5942a76251633aeda6fdc9d0db74357119300ba4
Emily Jena <jen21062@byui.edu>
2026-02-28 20:35:33 -0700

    Created a successful loop, end when 0 is entered.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,36 @@
 using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Globalization;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Heading
+        Console.WriteLine("Generating a Number List, Enter 0 to Finish");
+        // --------------------------------------
+  
+
+        //Add List items e.g python: numbers = []
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        while (number != 0)
+        {
+            Console.Write("Enter Number: ");
+             number = int.Parse(Console.ReadLine());
+        }
+
+        if (number != 0)
+        {
+            numbers.Add(number);          
+        }
+        // Compute Sum
+        
+        //Compute Average
+
+        //Compute Max
     }
 }
