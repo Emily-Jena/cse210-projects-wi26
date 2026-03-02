acfe1c0265ef0ee9a56ecbeb998bd7d43b1b81c5
Emily Jena <jen21062@byui.edu>
2026-03-02 09:58:34 -0700

    Used the class circle to create an instance.

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,32 +2,42 @@ using System;
 
 class Program
 {
-    // static void Main(string[] args)
-    // {
-    //     //Console.WriteLine("Hello Sandbox World!");
+    static void Main(string[] args)
+{
 
-    //     for (int i = 0; i < 10; i++)
-    //     {
-    //         Console.WriteLine(i);
-    //     }
-    // }
+    Console.WriteLine("Circle Class");
 
-// How to write a funtion
-    public static void Greeting()
-    {
-        Console.WriteLine("Hey Emily");
-    }
-// Returning a function 
-    public static int AddNumbers(int n1, int n2)
-    {
-        return n1 + n2;
-    }
+    // Class object = new 
+    Circle myCircle = new Circle();
 
-// Calling a function 
-static void Main(string[] args)
-    {
-        Greeting();
-        int total = AddNumbers(10,20);
-        Console.WriteLine(total);
-    }
+
+    myCircle.SetRadius(10);
+    Console.WriteLine(myCircle.GetRadius());
 }
+}
+//     // {
+//     //     //Console.WriteLine("Hello Sandbox World!");
+
+//     //     for (int i = 0; i < 10; i++)
+//     //     {
+//     //         Console.WriteLine(i);
+//     //     } 
+//     // }
+
+// // How to write a funtion
+//     public static void Greeting()
+//     {
+//         Console.WriteLine("Hey Emily");
+//     }
+// // Returning a function 
+//     public static int AddNumbers(int n1, int n2)
+//     {
+//         return n1 + n2;
+//     }
+
+// // Calling a function 
+// static void Main(string[] args)
+//     {
+//         Greeting();
+//         int total = AddNumbers(10,20);
+//         Console.WriteLine(total);
