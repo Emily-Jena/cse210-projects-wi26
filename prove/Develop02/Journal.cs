c88ac3a0b31194b3b5df6ecd24346d27015c9318
Emily Jena <jen21062@byui.edu>
2026-03-07 23:58:18 -0700

    Looping entries, read, save, & load files.

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,5 +1,42 @@
-class Journal
+public class Journal
 {
-    public List<JournalEntry> _entries;
+    public List<JournalEntry> _entries = new List<JournalEntry>();
 
+    public void AddEntry(JournalEntry entry)
+    {
+        _entries.Add(entry);
+    }
+
+    public void DisplayEntries()
+    {
+        foreach (JournalEntry entry in _entries)
+        {
+            entry.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (JournalEntry entry in _entries)
+            {
+               outputFile.WriteLine($"{entry._date}|{entry._prompt}|{entry._response}"); 
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();string[] lines = System.IO.File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split('|');
+            
+            JournalEntry newEntry = new JournalEntry(parts[0], parts[1], parts[2]);
+            _entries.Add(newEntry);
+        }
+
+    }
 }
