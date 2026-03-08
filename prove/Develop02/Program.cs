51e702def3a3c2ad5109e31f494236245802c61c
Emily Jena <jen21062@byui.edu>
2026-03-07 23:56:30 -0700

    Menu option created, working on a saving details.

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,10 +4,57 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Welcome to My Journal!");
+    
+
+    Journal myJournal = new Journal();
+    bool running = true;
+
+    while (running)
+    {
+        Console.WriteLine("Please select one of the following choices:");
+        Console.WriteLine("1. Write\n2. Display\n3. Load\n4. Save\n5. Quit");
+        string choice = Console.ReadLine();
+
     }
 
-    public string _title;
-    public string _description;
-    public string _menuItem;
+    switch (choice)
+    {
+        
+        case "1":
+        string date = DateTime.Now.ToShortDateString();
+                string prompt = "What was the best part of my day?"; 
+                Console.WriteLine($"\n{prompt}");
+                string response = Console.ReadLine();
+                
+                myJournal.AddEntry(new JournalEntry(date, prompt, response));
+        break;
+
+        case "2":
+            myJournal.DisplayEntries();
+        break;
+
+        case "3":
+            Console.Write("Enter filename: ");
+            string loadFile = Console.ReadLine();
+            myJournal.LoadFromFile(loadFile);
+        break;
+
+        case "4":
+            Console.Write("Enter filename: ");
+            string saveFile = Console.ReadLine();
+            myJournal.SaveToFile(saveFile);
+        break;
+
+        case "5":
+            running = false;
+        break;
+
+        default:
+        Console.WriteLine("You entered a wrong number. Please try again.");
+        break;
+    }
+
+   }
 }
+
