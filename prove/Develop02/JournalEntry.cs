5cfa3959e928c5b785c653e8f52fe31e078e7fd4
Emily Jena <jen21062@byui.edu>
2026-03-07 22:25:46 -0700

    JournalEntry added date, prompt, & response.

--- prove/Develop02/JournalEntry.cs
+++ prove/Develop02/JournalEntry.cs
@@ -3,4 +3,18 @@ class JournalEntry
     public string _date;
     public string _response;
     public string _prompt;
+
+    public JournalEntry(string date, string response, string prompt)
+    {
+        
+        _date = date;
+        _response = response;
+        _prompt = prompt;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Date: {_date} - Prompt: {_prompt}");
+        Console.WriteLine($"{_response}");
+    }
 }
