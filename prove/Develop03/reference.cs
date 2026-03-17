a031b2a97789b8907e4a4ecbbdf71c9d843502f5
Emily Jena <jen21062@byui.edu>
2026-03-17 09:58:39 -0600

    Files created

---
+++ prove/Develop03/reference.cs
@@ -0,0 +1,9 @@
+class Reference
+{
+    private string _book;
+    private int _chapter;
+    private int _verse;
+    private int _endVerse;
+    
+
+}
---
+++ prove/Develop03/scripture.cs
@@ -0,0 +1,5 @@
+class Scripture
+{
+    List<Word> _word = new List<Word>();
+    
+}
