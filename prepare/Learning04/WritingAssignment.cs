dc87e503ebf7d1ad0c77ab1b83c99f4ef55eec97
Emily Jena <jen21062@byui.edu>
2026-03-20 11:29:06 -0600

    Writing Assignment tasks completed

--- prepare/Learning04/WritingAssignment.cs
+++ prepare/Learning04/WritingAssignment.cs
@@ -1,5 +1,14 @@
-class WritingAssignment
+class WritingAssignment : Assignment
 {
     private string _title;
-    
+
+    public WritingAssignment(string studentName, string topic, string title) : base (studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return($"{_title} by {_studentName}");
+    }
 }
