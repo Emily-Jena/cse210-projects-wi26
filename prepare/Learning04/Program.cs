e4ec96e514f0f0369b6830e756541c41164d2e1d
Emily Jena <jen21062@byui.edu>
2026-03-20 11:30:33 -0600

    Added display content on program.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,10 +5,18 @@ class Program
     static void Main(string[] args)
     {
         // Create New Assignment & Call & Display
-        Assignment testAssignment = new Assignment("Samuel Bennett" , "multiplication");
+        Assignment testAssignment1 = new Assignment("Samuel Bennett" , "multiplication");
+        Console.WriteLine(testAssignment1.GetSummary());
+        Console.WriteLine();
 
-        string summary = testAssignment.GetSummary();
+        MathAssignment testAssignment2 = new MathAssignment ("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(testAssignment2.GetSummary());
+        Console.WriteLine(testAssignment2.GetHomeworkList());
+        Console.WriteLine();
 
-        Console.WriteLine(summary);
+        WritingAssignment testAssignment3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(testAssignment3.GetSummary());
+        Console.WriteLine(testAssignment3.GetWritingInformation());  
     }
+
 }
