a83a7e80d7336bb05ff4f8c2d506c393a286b3e6
Emily Jena <jen21062@byui.edu>
2026-03-21 20:55:14 -0600

    Base class has been updated

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -5,13 +5,44 @@ class Activity
     protected int _duration;
 
     // constructor
-    public Activity(string name, string description, int duration)
+    public Activity(string name, string description)
     {
         _name = name;
         _description = description;
-        _duration = duration;
-
     }
 
-    
+    public void DisplayStartingMessage()
+    {
+        Console.Clear();
+        Console.WriteLine($"Welcome to the {_name}.");
+        Console.WriteLine($"{_description}");
+    }
+
+    public void DisplayEndingMessage()
+    {
+        Console.WriteLine($"Well done");
+        ShowSpinner(3);
+
+        Console.WriteLine($"You have completed {_duration} seconds of the {_name}.");
+        ShowSpinner(5);
+    }
+
+    public void ShowSpinner(int seconds)
+    {
+        
+    }
+
+    public void ShowCountDown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i);
+            Thread.Sleep(1000);
+            Console.Write("\b \b");
+        }
+    }
+    public void Run()
+    {
+        
+    }
 }
