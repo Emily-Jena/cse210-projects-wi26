147241a45efe4e83b7fb7bac27a1ab94ebfa6428
Emily Jena <jen21062@byui.edu>
2026-03-23 09:37:37 -0600

    classes file created. Completed base shape class

---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,27 @@
+class Shape
+{
+    // class member variable & constructor
+    private string _color;
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    // getter & setter
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    // virtual method
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
