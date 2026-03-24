1e16212fdc3a3d2a35c9fee808dfa7b63962d399
Emily Jena <jen21062@byui.edu>
2026-03-23 22:04:32 -0600

    Built a list in program and updated in circle.

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,25 +1,35 @@
 using System;
+using System.Drawing;
 
 class Program
 {
     static void Main(string[] args)
     {
+
+        List<Shape> shapes = new List<Shape>();
         //Square
         Square s1 = new Square("Red", 5);
-
-        // string  color = s1.GetColor();
-        // double area = s1.GetArea();
-
-        Console.WriteLine($"The square color is {s1.GetColor()} and the area is {s1.GetArea()}.");
+        shapes.Add(s1);
 
         // Rectangle
         Rectangle s2 = new Rectangle("blue", 2, 3);
-
-        // string color = s2.GetColor();
-        // double area = s2.GetArea();
-
-        Console.WriteLine($"The rectangle color is {s2.GetColor()} and the area is {s2.GetArea()}.");
+        shapes.Add(s2);
 
         //Circle
+        Circle s3 = new Circle("White", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The circle color is {color} and the area is {area}.");
+        }
+        
+
+        
+
+
     }
 }
