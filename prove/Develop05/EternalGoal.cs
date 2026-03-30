f36ab4cebdb59a509af60eedd6b5461adbb3a84c
Emily Jena <jen21062@byui.edu>
2026-03-30 09:30:40 -0600

    eternalGoal class created

---
+++ prove/Develop05/EternalGoal.cs
@@ -0,0 +1,23 @@
+public class EternalGoal : Goal
+{
+    public EternalGoal(string name, string description, int points) : base(name, description, points)
+    {
+        
+    }
+
+    // Never Complete
+    public override void RecordEvent()
+    {
+        
+    }
+
+    public override bool IsComplete()
+    {
+        return false;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"EternalGoal: {_name}, {_description}, {_points}";
+    }
+}
