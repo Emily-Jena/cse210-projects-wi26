b051b9b5ddec6ff02880a3ef3c0d4183276f4dce
Emily Jena <jen21062@byui.edu>
2026-03-30 09:31:46 -0600

    base class goal created.

---
+++ prove/Develop05/Goal.cs
@@ -0,0 +1,30 @@
+using System.Runtime.CompilerServices;
+
+public abstract class Goal
+{
+    protected string _name;
+    protected string _description;
+    protected int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _name = name;
+        _description = description;
+        _points = points;
+    }
+// abstract methods
+    public abstract void RecordEvent();
+
+    // See if goal is done
+    public abstract bool IsComplete();
+
+    // To save to file
+    public abstract string GetStringRepresentation();
+    
+// virtual methods- goal appear on list
+    public virtual string GetDetailsString()
+    {
+        string statusSymbol = IsComplete() ? "[X]" : "[ ]";
+        return $"{statusSymbol} {_name} ({_description})";
+    }
+} 
