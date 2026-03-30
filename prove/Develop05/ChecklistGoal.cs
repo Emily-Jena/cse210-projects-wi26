3a57a3f168445e46c99ba292475eedd9750c090b
Emily Jena <jen21062@byui.edu>
2026-03-30 09:30:53 -0600

    checklistGoal class created

---
+++ prove/Develop05/ChecklistGoal.cs
@@ -0,0 +1,34 @@
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+    private int _target;
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+
+    public override void RecordEvent()
+    {
+        _amountCompleted++;
+    }
+    public override bool IsComplete()
+    {
+        return _amountCompleted >= _target;
+    }
+
+    // total repeat
+    public override string GetDetailsString()
+    {
+       string statusSymbol = IsComplete() ? "[X]" : "[ ]";
+       return $"{statusSymbol} {_name} ({_description}) -- Completed: {_amountCompleted}/{_target}";
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"ChecklistGoal: {_name}, {_description}, {_points}, {_bonus}, {_target}, {_amountCompleted}";
+    }
+}
