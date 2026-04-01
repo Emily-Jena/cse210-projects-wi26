57b19e7015436a543f7668c5b2eb854a4f3655b1
Emily Jena <jen21062@byui.edu>
2026-04-01 10:28:03 -0600

    Simplified Process Activity

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -5,21 +5,35 @@ public class BreathingActivity : Activity
     public BreathingActivity() : base("Breathing Activity", "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.")
     {}
 
-    public void BreathingActivity.Run()
+    public void Run()
     {
-        int cycleTime = 10;
-        int totalCycles = _duration / cycleTime;
+        // Start Activity
+        DisplayStartingMessage();
 
-        for (int i = 0; i < totalCycles; i++)
+        DateTime startTime = DateTime.Now;
+        DateTime endtime = startTime.AddSeconds(_duration);
+
+        // Process activity
+
+        
+        // int cycleTime = 10;
+        // int totalCycles = _duration / cycleTime;
+
+        // for (int i = 0; i < totalCycles; i++)
+        // {
+        while (DateTime.Now < endtime)
         {
+            Console.WriteLine();
             Console.Write("\n Breathe In... ");
             ShowCountDown(5);
 
+            Console.WriteLine();
             Console.Write("\n Breathe Out... ");
             ShowCountDown(5);
             Console.WriteLine();
         }
 
+        // End Activity
         DisplayEndingMessage();
     }
-}
+} 
