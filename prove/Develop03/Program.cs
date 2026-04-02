f7e8f3ff4c89cdc763594edda0a16acc095adae6
Emily Jena <jen21062@byui.edu>
2026-04-02 10:14:27 -0600

    Updated program functionality and updated scripture

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -4,16 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Word myWord= new Word("Testing");
-        Console.WriteLine(myWord.GetwordString());
+        Reference reference = new Reference("1 Nephi", 1, 5, 6);
 
-        myWord.HideWord();
-        Console.WriteLine(myWord.GetwordString());
-// --------------------------------------------------
-        Word myWord2= new Word("Moroni,;:");
-        Console.WriteLine(myWord2.GetwordString());
+        // Create Scripture
+        string text = "I, Nephi, having been born of goodly parents, therefore I was taught somewhat in all the learning of my father; and having seen many afflictions in the course of my days, nevertheless, having been highly favored of the Lord in all my days; yea, having had a great knowledge of the goodness and the mysteries of God, therefore I make a record of my proceedings in my days.";
 
-        myWord2.HideWord();
-        Console.WriteLine(myWord2.GetwordString());
+        Scripture scripture = new Scripture(reference, text);
+        string input = " ";
+
+        // Loop
+        while (input.ToLower() != "quit" && !scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplaytext());
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue or type 'quit' to finish: ");
+
+            input = Console.ReadLine();
+
+            if (input.ToLower() != "quit")
+            {
+                scripture.HideRandomWords(5); //Hidding total number of...
+            }
+        }
+
+        Console.Clear();
+        Console.WriteLine(scripture.GetDisplaytext());
+        Console.WriteLine("\n All words are hidden. Well done!");
+
     }
 }
