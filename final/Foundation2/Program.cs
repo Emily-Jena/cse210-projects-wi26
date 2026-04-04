f8391595abb43f1e6987a88d2c3d7e145956d325
Emily Jena <jen21062@byui.edu>
2026-04-04 15:55:16 -0600

    Display Ordering to user

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -4,16 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // List 
-        List<Product> catalog = new List<Product>
-        {
-            new Product("Laptop", "L100", 1099.99, 1),
-            new Product("Keyboard", "K240", 30.00, 1),
-            new Product("HDMI", "C4", 5.50, 1),
-            new Product("Mouse", "M25", 20.00, 1),
-            new Product("Screen Cleaner Kit", "S500", 35.99, 1)
-        };
-
+        
         Console.Clear();
         Console.WriteLine(" ~ Welcome to the Online Ordering Store ~ ");
         Console.WriteLine("Please enter the following details: ");
@@ -26,13 +17,66 @@ class Program
         Console.Write("State/Province: "); string state = Console.ReadLine();
         Console.Write("Country: "); string country = Console.ReadLine();
 
+        Console.Clear();
         Console.WriteLine("------------------------------------------------");
         Console.WriteLine();
 
-        // Create objects with user personal info & order
+         // Create objects with user personal info & order
         Address userAddress = new Address(street, city, state, country);
         Customer userCustomer = new Customer(name, userAddress);
         Order userOrder = new Order(userCustomer);
+
+        // List 
+        List<Product> catalog = new List<Product>
+        {
+            new Product("Laptop", "L100", 1099.99, 1),
+            new Product("Keyboard", "K240", 30.00, 1),
+            new Product("HDMI", "C4", 5.50, 1),
+            new Product("Mouse", "M25", 20.00, 1),
+            new Product("Screen Cleaner Kit", "S500", 35.99, 1)
+        };
+
+        for (int i = 0; i < catalog.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {catalog[i].GetName()} - ${catalog[i].GetTotalCost():0.00}");
+            }
+            Console.WriteLine("0. Complete Checkout");
+
+
+        bool addingProducts = true;
+        while (addingProducts)
+        {
+            
+            Console.WriteLine("------------------------------------------------");
+            
+            Console.Write("Select a product number to add to your cart: ");
+            
+
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "0":
+                    // 
+                    // Console.Clear();
+                    addingProducts = false;
+                break;
+
+                default:
+                    if (int.TryParse(choice, out int index) && index > 0 && index <= catalog.Count)
+                    {
+                        Product selected = catalog[index - 1];
+                        userOrder.AddProduct(selected);
+                        Console.WriteLine($"ADDED ~ {selected.GetName()} ~ to your cart.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid number. Try again.");
+                    }
+                    break;
+            }
+        }
+
 
         // Display Receipt 
         Console.Clear();
@@ -59,11 +103,6 @@ class Program
         Console.WriteLine("===========================================================");
         Console.WriteLine("Thank you for ordering! Do Visit Us Again!");
         Console.WriteLine("===========================================================");
-        
-
-
-
-
 
     }
 }
