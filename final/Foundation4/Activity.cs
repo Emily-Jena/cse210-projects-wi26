d3242728ef461f8f8cb1c38926803fa243b32f45
Emily Jena <jen21062@byui.edu>
2026-04-04 23:56:57 -0600

    Base Activity Created.

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -1,4 +1,24 @@
-public class Activity
+public abstract class Activity
 {
-    
+    private string _date;
+    private int _minutes;
+
+    public Activity(string date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public int GetMinutes() => _minutes;
+
+    // virtual methods to override
+    public virtual double GetDistance() => 0;
+    public virtual double GetSpeed() => 0;
+    public virtual double GetPace() => 0;
+
+    public string GetSummary()
+    {
+        
+
+    }
 }
