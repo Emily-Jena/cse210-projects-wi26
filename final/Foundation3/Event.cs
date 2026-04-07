512a0bdbfb9998b3d059cd8979142914f6224234
Emily Jena <jen21062@byui.edu>
2026-04-07 05:08:18 -0600

    Event details added for functionality

--- final/Foundation3/Event.cs
+++ final/Foundation3/Event.cs
@@ -7,22 +7,27 @@ public class Event
     private string _date;
     private string _time;
     private Address _address;
+    
 
     public Event(string title, string description, string date, string time, Address address)
     {
-        
+         _title = title;
+         _description = description;
+         _date = date;
+         _time = time;
+         _address = address;
     }
     public string GetStandardDetails()
     {
-        
+        return $"Title: {_title} \nDescription: {_description} \nDate: {_date} at {_time} \nLocation: {_address.GetFullAddress()}";
     }
     public string GetShortDescription()
     {
-        
+        return $"Event Type: General \nTitle: {_title} \nDate: {_date}";
     }
 
     public virtual string GetFullDetails()
     {
-        
+        return GetStandardDetails();
     }
 }
