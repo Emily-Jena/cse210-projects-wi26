5806f596f9a7c2430e8b5997835300d12da3488d
Emily Jena <jen21062@byui.edu>
2026-04-07 03:47:41 -0600

    GetSummary added

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -18,7 +18,7 @@ public abstract class Activity
 
     public string GetSummary()
     {
-        
+        return $"{_date} - {GetType().Name} ({_minutes} min): " + $"Distance - {GetDistance(): 0.0}, Speed - {GetSpeed(): 0.0}, Pace - {GetPace(): 0.0}";
 
     }
 }
