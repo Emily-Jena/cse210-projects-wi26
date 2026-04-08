c0c148d9f8b555384ef6ef9136b1e0d0ae43096f
Emily Jena <jen21062@byui.edu>
2026-04-07 19:52:32 -0600

    Completed menu display

--- prove/Develop02/menu.cs
+++ prove/Develop02/menu.cs
@@ -3,7 +3,17 @@ public class Menu
     public string[] _menuItems = 
     {
         "Write", "Display", "Load", "Save", "Quit"
-    };
+    }; //string list available
+ 
+    public void DisplayMenu()
+    {
+        Console.WriteLine($"Select one of the following Menu options: ");
 
-    
+        for (int i = 0; i < _menuItems.Length; i++)
+        {
+            Console.WriteLine($"{i + 1}. {_menuItems[i]}");
+        }
+
+        Console.Write($"What do you want to do? ");
+    }
 }
